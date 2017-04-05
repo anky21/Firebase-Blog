325ab6cb213bc1f35eaa5b20b59fdd924e1da8ce
Anky <anky_au@hotmail.com>
2017-04-05 17:06:03 +1000

    feat: Remove MyPosts and TopPosts

--- app/src/main/java/com/google/firebase/quickstart/database/MainActivity.java
+++ app/src/main/java/com/google/firebase/quickstart/database/MainActivity.java
@@ -27,8 +27,6 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.quickstart.database.fragment.MyPostsFragment;
-import com.google.firebase.quickstart.database.fragment.MyTopPostsFragment;
 import com.google.firebase.quickstart.database.fragment.RecentPostsFragment;
 
 public class  MainActivity extends BaseActivity {
@@ -47,13 +45,9 @@ public class  MainActivity extends BaseActivity {
         mPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
             private final Fragment[] mFragments = new Fragment[] {
                     new RecentPostsFragment(),
-                    new MyPostsFragment(),
-                    new MyTopPostsFragment(),
             };
             private final String[] mFragmentNames = new String[] {
                     getString(R.string.heading_recent),
-                    getString(R.string.heading_my_posts),
-                    getString(R.string.heading_my_top_posts)
             };
             @Override
             public Fragment getItem(int position) {
